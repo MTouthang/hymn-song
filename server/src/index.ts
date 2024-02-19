c973f9dbf3d328d58cf4a0627564f5608ab223ff
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-02-20 00:33:39 +0530

    feat: swagger setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express"
+import fs from "fs"
+import YAML from 'yaml'
+
 
 const app = express();
+
+
 const PORT = process.env.PORT || 3000;
+
+// swagger
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript & Express..!');
