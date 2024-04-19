3dfc8af3eb10efce2775d0c549918038783a3187
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-04-20 00:30:02 +0530

    feat: arrow functionality

--- client/src/pages/Lyric.tsx
+++ client/src/pages/Lyric.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ILyric } from '../types';
 import { useLyricContext } from '../context/LyricContext';
@@ -7,24 +7,73 @@ interface LyricProps {
   data: ILyric ; // Explicitly type the data prop
 }
 const Lyric: React.FC<LyricProps> = ({ data }) => {
-  const [slide, setSlide] = useState<string>(data.verses[0].lyrics);
-
   const { lyricData } = useLyricContext();
+  const [slide, setSlide] = useState<string>(lyricData.verses[0]?.lyrics || '');
+  const [index, setIndex] = useState<number>(0);
 
-  const handleSlide = (item: string) => {
-    setSlide(item);
-  };
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'ArrowUp':
+          if (index > 0) {
+            setIndex((prevIndex) => prevIndex - 1);
+          }
+          break;
+        case 'ArrowDown':
+          if (index < lyricData.verses.length - 1) {
+            setIndex((prevIndex) => prevIndex + 1);
+          }
+          break;
+        
+        case "ArrowLeft":
+          if(lyricData.chorus) {
+            setSlide(data.chorus)
+          }
+          break
+        
+        case "ArrowRight":
+          if(lyricData.chorus) {
+            setSlide(data.chorus)
+          }
+          break
+
+        default:
+          break;
+      }
+    };
+
+    // Attach event listener when component mounts
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Clean up by removing event listener when component unmounts
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [index, lyricData.verses.length]); // Re-run effect when index or verse length changes
+
+  // Update slide (lyrics) when index changes
+  useEffect(() => {
+    setSlide(lyricData.verses[index]?.lyrics || '');
+  }, [index, lyricData.verses]);
+
+  const handleSlide = (index:number) => {
+    setIndex(index)
+    
+  }
 
   return (
     <>
       <div className="flex items-center w-full py-10 mx-auto 2xl:w-5/6 border-dark-100 ">
         <div className="items-center justify-center w-32 2xl:w-40 bg-dark-50 ">
           <ul className="items-center text-left">
-            {lyricData.verses.map((item) => (
+            {lyricData.verses.map((item, index) => (
               <li 
                 className='text-gray-400 hover:cursor-pointer hover:text-gray-700'
                 key={item.verseNumber}
-                onClick={() => handleSlide(item.lyrics)}
+                onClick={() => {
+                  handleSlide( index)
+                }}
               >
                 {item.verseNumber}. {item.lyrics.slice(0, 10)}...
               </li>
